f564a877b365528799397ef7c9ef75e67f048df3
sophondetector <nateisgood@gmail.com>
2024-09-03 19:08:58 -0400

    add cgi-bin stub and build out podman-tools.sh

--- devops/podman-tools.sh
+++ devops/podman-tools.sh
@@ -1,5 +1,43 @@
+#!/usr/bin/env bash
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+        ;;
+        *)
+            false
+        ;;
+    esac
+}
+
 # list containers
-# create jraph-service container
-# ssh into container
+podlist () {
+	podman container list --all "$@"
+}
+
 # clear containers
+podclear () {
+	confirm "clear all podman containers?" \
+		&& podman container rm --volumes "$@" \
+		|| echo "didn't do anything"
+}
+
+# create jraph-service container
+# TODO add options
+podmake () {
+	podman run -it fedora:latest /bin/bash
+}
+
+# ssh into container
+podssh () {
+	podman exec -it $1 /bin/bash
+}
+
 # jraph service status
+podcheck () {
+	podman container inspect $1
+}
+
