e873feaddf0a4ba0412665c47545be9c9e0d02f2
sophondetector <nateisgood@gmail.com>
2024-09-05 15:50:22 -0400

    simplify test-sql

--- devops/test-sql.sh
+++ devops/test-sql.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-# Test command for .sql scripts to be run from the jraph service host machine/container
+echo "testing sql service"
 
-DB=jongraph
-OUTPUT=out.log
-INPUT=${1}
-PASSWORD=${PASSWORD:-}
-SQLUSER=sa
-
-HELP_MESSAGE="$ testSql.sh path/to/input.sql; output in out.log; requires sqlcmd to be installed"
-
-if [[ "$@" =~ \-h\ ? || "$@" =~ \-\-help\ ? ]]; then
-	echo $HELP_MESSAGE
-	return
-fi
-
-echo $DB
-echo $OUTPUT
-echo $INPUT
-echo $PASSWORD
-echo $SQLUSER
-
-sqlcmd -C -U $SQLUSER -P $PASSWORD -d $DB -i $INPUT -o $OUTPUT
+sqlcmd \
+	-C \
+	-U sa \
+	-P $JRAPH_SA_PASSWORD \
+	-d jongraph \
+	-i test.sql \
+	-o out.log
 
