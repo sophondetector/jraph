8e00ba852a61ca0d7c63b103f3cc12f9fb6bc46e
sophondetector <nateisgood@gmail.com>
2024-09-05 13:04:27 -0400

    remove secrets.sh and add config.EXAMPLE.sh

--- devops/secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export JRAPH_ADMIN_USERNAME="mradmin"
-export JRAPH_ADMIN_PASSWORD="Intrototerror1!"
-export JRAPH_ROOT_PASSWORD="Intrototerror1!"
-export JRAPH_SA_PASSWORD="Intrototerror1!"
-
