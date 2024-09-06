c039a7f647c4322621004ecb3b98ec665796babd
sophondetector <nateisgood@gmail.com>
2024-09-06 09:42:17 -0400

    make more clear what's going on with name

---
+++ JROOT/root/install-sqlserver.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DEPS="mssql-server mssql-tools18 unixODBC-devel"
+
+echo "installing following SQL Server deps: $DEPS"
+
+curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/9/mssql-server-2022.repo
+curl -o /etc/yum.repos.d/mssql-release.repo https://packages.microsoft.com/config/rhel/9/prod.repo
+dnf check-update
+dnf install -y $DEPS
+dnf update --all
+
+# TODO configgify the interactive answers
+echo "configuring mssql-server service"
+/opt/mssql/bin/mssql-conf setup
+
+echo "making sqlcmd avail in PATH"
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+
