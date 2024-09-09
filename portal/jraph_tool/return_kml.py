e65ddec866b6af37936a28d273e526540308f852
sophondetector <nateisgood@gmail.com>
2024-09-09 17:16:05 -0400

    working return_kml stub

--- portal/jraph_tool/return_kml.py
+++ portal/jraph_tool/return_kml.py
@@ -1,11 +1,28 @@
+import json
 from dbc import init_jraph_conn
 import simplekml
 
 with init_jraph_conn() as conn:
     cur = conn.cursor()
+
     # 3 is the MIIS id
     cur.execute("SELECT * FROM node WHERE node_id=3;")
+
     row = cur.fetchone()
+
     kml = simplekml.Kml()
-    pnt = kml.newpoint(**row)
+
+    raw = json.loads(row[1])
+
+    pnt = kml.newpoint(
+        name=raw['name'],
+        description=raw['type'],
+        coords=[(raw['lat'], raw['long'])]
+    )
+
     print(kml.kml())
+
+    # kml.save("output.kml")
+    # print("saved to output.kml")
+
+    print("finito")
