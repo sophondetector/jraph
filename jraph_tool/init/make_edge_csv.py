f64f69143d0d2916958bd10947a32a64a59bf5b3
sophondetector <nateisgood@gmail.com>
2024-09-17 17:01:21 -0400

    add csv script

--- jraph_tool/init/make_edge_csv.py
+++ jraph_tool/init/make_edge_csv.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 _EDGE_CSV = '~/Public/Datasets/offshore-leaks-db/relationships.csv'
 _US_ENTITY_CSV = 'lib/data/init_offshore_data.csv'
-_OUTPUT_FP = 'output.csv'
+_OUTPUT_FP = 'lib/data/init_offshore_edges.csv'
+
+
+def _load_us_edges():
+    print(f'loading offshore leaks data from {_EDGE_CSV}')
+    return pd.read_csv(_OUTPUT_FP, index_col=False)
 
 
 def _load_edge():
@@ -20,17 +25,24 @@ def _save_output(df):
     print('save to {} success'.format(_OUTPUT_FP))
 
 
-def _process(df):
-    print('processing dataframe')
-    return df
-
-
-def main(*args, **kwargs):
-    return 0
-
-
 if __name__ == '__main__':
+    print('loading edge and us entity csvs')
     edge_df = _load_edge()
     use_df = _load_us_ents()
-    # out_df = _process(edge_df, use_df)
-    # _save_output(out_df)
+
+    print('merging and us entity csvs')
+    start_in_us = edge_df.merge(
+        use_df['node_id'], how='inner',
+        left_on='_start', right_on='node_id'
+    )
+    end_in_us = edge_df.merge(
+        use_df['node_id'], how='inner',
+        left_on='_end', right_on='node_id'
+    )
+
+    out = pd.concat([start_in_us, end_in_us])
+    out.drop('node_id', axis=1, inplace=True)
+    out.drop_duplicates(inplace=True)
+    # TODO drop dups!!!
+
+    _save_output(out)
