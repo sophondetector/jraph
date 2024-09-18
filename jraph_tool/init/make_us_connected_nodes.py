4d345e2482fd49434fbf0301e32643980f7396fc
sophondetector <nateisgood@gmail.com>
2024-09-18 14:50:52 -0400

    global var change

--- jraph_tool/init/make_us_connected_nodes.py
+++ jraph_tool/init/make_us_connected_nodes.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 print('making csv of nodes connected to us nodes')
 
-_UBER_CSV = '~/Public/Datasets/offshore-leaks-db/nodes-entities.csv'
-_INIT_EDGES_CSV = 'lib/data/init_offshore_edges.csv'
-_US_ENTITY_CSV = 'lib/data/init_offshore_data.csv'
-_OUTPUT_FP = 'lib/data/us_connected_nodes.csv'
+UBER_CSV = '~/Public/Datasets/offshore-leaks-db/nodes-entities.csv'
+INIT_EDGES_CSV = 'lib/data/init_offshore_edges.csv'
+US_ENTITY_CSV = 'lib/data/init_offshore_data.csv'
+OUTPUT_FP = 'lib/data/us_connected_nodes.csv'
 
-udf = pd.read_csv(_UBER_CSV)
-edf = pd.read_csv(_INIT_EDGES_CSV)
-usdf = pd.read_csv(_US_ENTITY_CSV)
+udf = pd.read_csv(UBER_CSV)
+edf = pd.read_csv(INIT_EDGES_CSV)
+usdf = pd.read_csv(US_ENTITY_CSV)
 
 node_ids = set()
 for v in edf._start.values:
@@ -23,6 +23,6 @@ node_index = pd.Index(node_ids)
 out = udf.filter(node_index, axis=0)
 # out = out[out.country_codes.apply(lambda cc: 'USA' not in cc)]
 
-out.to_csv(_OUTPUT_FP, index=False)
+out.to_csv(OUTPUT_FP, index=False)
 
-print(f'save to {_OUTPUT_FP} done')
+print(f'save to {OUTPUT_FP} done')
