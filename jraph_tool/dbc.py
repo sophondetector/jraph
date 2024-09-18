e042f6c8a2f992567d95a2ef255c0e539a10bd21
sophondetector <nateisgood@gmail.com>
2024-09-18 14:50:20 -0400

    put db funcs in dbc

--- jraph_tool/dbc.py
+++ jraph_tool/dbc.py
@@ -1,7 +1,11 @@
 import os
+import json
 import pyodbc
 
+from typing import Optional, Union
+
 from jraph_tool import Node
+from jraph_tool.init.init_utils import nan2none
 
 _CONN = None
 
@@ -45,3 +49,49 @@ def query_node(node_id: int) -> Node:
         cur.execute("SELECT * FROM node WHERE node_id=?;", node_id)
         node_id, properties_raw = cur.fetchone()
         return Node(node_id, properties_raw)
+
+
+def insert_node(
+        nid,
+        props: Optional[Union[dict, str]] = None,
+        cur: Optional[pyodbc.Cursor] = None):
+
+    if cur is None:
+        cur = get_cur()
+
+    if type(props) is dict:
+        props = json.dumps(props)
+        props = nan2none(props)
+        # TODO use a better json encoder for this
+
+    with cur:
+        cur.execute('SET IDENTITY_INSERT node ON;')
+        cur.execute("INSERT node (node_id, properties) VALUES (?, ?)",
+                    nid, props)
+        cur.commit()
+        return cur.fetchone()
+
+
+def insert_edge(
+        source_id: int,
+        target_id: int,
+        props: Optional[Union[dict, str]] = None,
+        cur: Optional[pyodbc.Cursor] = None):
+
+    if cur is None:
+        cur = get_cur()
+
+    if props is None:
+        props = '{}'
+
+    if type(props) is dict:
+        props = json.dumps(props)
+        props = nan2none(props)
+        # TODO use a better json encoder for this
+
+    with cur:
+        cur.execute(
+            "INSERT edge (source_id, target_id, properties) VALUES (?, ?, ?)",
+            source_id, target_id, props)
+        cur.commit()
+        return cur.fetchone()
--- jraph_tool/init/init_edge_db.py
+++ jraph_tool/init/init_edge_db.py
@@ -1,62 +1,30 @@
-import json
 import pandas as pd
 import jraph_tool.dbc as dbc
-import jraph_tool.init.init_utils as ut
-
-_EDGE_CSV = 'lib/data/init_offshore_edges.csv'
-_DF = pd.read_csv(_EDGE_CSV)
-
-_STOP = 10
 
 
-def insert_node(nid, props, cur=None):
-    if cur is None:
-        cur = dbc.get_cur()
+EDGE_CSV = 'lib/data/init_offshore_edges.csv'
+DF = pd.read_csv(EDGE_CSV)
+STOP = -1
+SUCCESS = 0
+ERROR = 0
 
-    if type(props) is dict:
-        props = json.dumps(props)
-        props = ut.nan2none(props)
+for idx, row in DF.iterrows():
+    print('DOING ', idx)
+    props_only = row.drop(['_start', '_end', '_type'])
+    props_string = props_only.to_json()
 
-    with cur:
-        cur.execute('SET IDENTITY_INSERT node ON;')
-        cur.execute("INSERT node (node_id, properties) VALUES (?, ?)",
-                    nid, props)
-        cur.commit()
-        return cur.fetchone()
+    try:
+        res = dbc.insert_edge(row._start, row._end, props_string)
+        SUCCESS += 1
+    except Exception as e:
+        ERROR += 1
+        print('error at row {}'.format(idx))
+        print(e)
 
+    if idx == STOP:
+        break
 
-def insert_edge(source_id, target_id, props=None, cur=None):
-    if cur is None:
-        cur = dbc.get_cur()
-
-    if props is None:
-        props = '{}'
-
-    if type(props) is dict:
-        props = json.dumps(props)
-        props = ut.nan2none(props)
-
-    with cur:
-        cur.execute(
-            "INSERT edge (source_id, target_id, properties) VALUES (?, ?, ?)",
-            row._start, row._end, props)
-        cur.commit()
-        return cur.fetchone()
-
-
-try:
-    for idx, row in _DF.iterrows():
-        print('DOING ', idx)
-        print(row)
-        props_only = row.drop(['_start', '_end', '_type'])
-        props_string = props_only.to_json()
-        insert_edge(row._start, row._end, props_string)
-
-        if idx == _STOP:
-            break
-
-except Exception as e:
-    print(e)
-
-finally:
-    dbc.get_conn().close()
+dbc.get_conn().close()
+print('attempted {} rows'.format(idx))
+print('{} successful'.format(SUCCESS))
+print('{} errors'.format(ERROR))
