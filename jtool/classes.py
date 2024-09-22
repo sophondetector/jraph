b82bda3410d1faf49e6d94121829655295f09d83
sophondetector <nateisgood@gmail.com>
2024-09-22 18:20:47 -0400

    add better repr to classes

--- jtool/classes.py
+++ jtool/classes.py
@@ -2,16 +2,34 @@ from typing import Optional
 
 
 class Node:
-    def __init__(self, node_id: int, properties: Optional[dict] = None, **kwargs):
+    def __init__(
+        self,
+        node_id: int,
+        properties: Optional[dict] = None,
+        **kwargs
+    ):
         self.id = node_id
         self.properties = properties if properties is not None else dict()
         self.properties.update(**kwargs)
 
+    def __repr__(self):
+        return f'<Node {self.id}>'
+
 
 class Edge:
-    def __init__(self, edge_id: int, source_id: int, target_id: int, properties: Optional[dict] = None, **kwargs):
+    def __init__(
+        self,
+        edge_id: int,
+        source_id: int,
+        target_id: int,
+        properties: Optional[dict] = None,
+        **kwargs
+    ):
         self.id = edge_id
         self.source_id = source_id
         self.target_id = target_id
         self.properties = properties if properties is not None else dict()
         self.properties.update(**kwargs)
+
+    def __repr__(self):
+        return f'<Edge {self.id}>'
