2e60ca305438529eb69c8b24c43719223c024e37
sophondetector <nateisgood@gmail.com>
2024-09-23 13:29:12 -0400

    make class names less confusing

--- jtool/classes.py
+++ jtool/classes.py
@@ -8,12 +8,12 @@ class Node:
         properties: Optional[dict] = None,
         **kwargs
     ):
-        self.id = node_id
+        self.node_id = node_id
         self.properties = properties if properties is not None else dict()
         self.properties.update(**kwargs)
 
     def __repr__(self):
-        return f'<Node {self.id}>'
+        return f'<Node {self.node_id}>'
 
 
 class Edge:
@@ -25,11 +25,11 @@ class Edge:
         properties: Optional[dict] = None,
         **kwargs
     ):
-        self.id = edge_id
+        self.edge_id = edge_id
         self.source_id = source_id
         self.target_id = target_id
         self.properties = properties if properties is not None else dict()
         self.properties.update(**kwargs)
 
     def __repr__(self):
-        return f'<Edge {self.id}>'
+        return f'<Edge {self.edge_id}>'
