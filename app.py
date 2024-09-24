23a87f1d5b4961052a694153538e430c6f792159
sophondetector <nateisgood@gmail.com>
2024-09-23 20:46:28 -0400

    add previous query back into it, janky

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask("jraph")
 
 DEFAULT_OUTPUT = "No output yet"
 LAST_OUTPUT = DEFAULT_OUTPUT
+PREVIOUS_QUERIES = []
 
 
 @app.route("/download", methods=["GET"])
@@ -23,26 +24,37 @@ def download():
 
 @app.route("/", methods=["GET"])
 def index():
-    global LAST_OUTPUT
+    global LAST_OUTPUT, PREVIOUS_QUERIES
     arg_list = list(request.args.items())
-    print('ARGS ', arg_list)
-    if len(arg_list) < 1:
-        return render_template("index.html", output=LAST_OUTPUT)
+    filter_args = list(
+        filter(lambda r: r[1], arg_list)
+    )
+    print('ARGS ', filter_args)
+    if len(filter_args) < 1:
+        LAST_OUTPUT = ''
+        return render_template(
+            "index.html",
+            output=LAST_OUTPUT,
+            previous_queries=PREVIOUS_QUERIES
+        )
 
     nodes = []
+    this_query = ''
     for k, v in arg_list:
+        if not v:
+            continue
+        this_query += '{}: {}|'.format(k.strip(), v.strip())
         if k == "node_id":
             node_row = [query_node(v)]
-        elif len(v) > 0:
-            node_row = query_node_prop(k, v)
         else:
-            node_row = []
+            node_row = query_node_prop(k, v)
         nodes.extend(node_row)
+    this_query = this_query[:-1]
 
     jr = Jraph()
     seen = set()
     for n in nodes:
-        # TODO track this down
+        # TODO why there are Nones here sometimes?
         if n is None:
             continue
         if n.node_id in seen:
@@ -50,5 +62,10 @@ def index():
         seen.add(n.node_id)
         jr.add(n)
 
+    PREVIOUS_QUERIES.append(this_query)
     LAST_OUTPUT = jr.j2k().kml()
-    return render_template("index.html", output=LAST_OUTPUT)
+    return render_template(
+        "index.html",
+        output=LAST_OUTPUT,
+        previous_queries=PREVIOUS_QUERIES
+    )
