e088e5532ad4f42eeba80dacf3b76df22a2e9b89
sophondetector <nateisgood@gmail.com>
2024-10-02 13:24:15 -0400

    still not working but more organized

--- jtool/parse_address.py
+++ jtool/parse_address.py
@@ -3,7 +3,86 @@ from typing import Optional
 import re
 import json
 
-from .utils import reverse_zip_parse, UsStateTools, safe_clean_string, JParse
+import pandas as pd
+
+_REVERSE_ZIP_REGEX = re.compile(r'(\d\d\d\d\-?)? ?(\d\d\d\d\d)')
+_USA_REGEX = re.compile(
+    r'\b(usa|u s a|united states of america)\b(si)?', re.IGNORECASE)
+_NAME_REGEX = re.compile(r'^[^\d]+')
+_PO_BOX_REGEX_NO_NUM = re.compile(r'p\.?o\.?\s+box', re.IGNORECASE)
+_PO_BOX_REGEX = re.compile(r'p\.?o\.?\s+box\s+[a-z\-]*\d+', re.IGNORECASE)
+_STREET_TERM_REGEX = re.compile(
+    r'.*(street|avenue|boulevard|highway|st|ave?|blvd)[\.,]?',
+    re.IGNORECASE
+)
+
+
+_US_STATE_TUPLES = [
+    ("alabama", "al"),
+    ("kentucky", "ky"),
+    ("ohio", "oh"),
+    ("alaska", "ak"),
+    ("louisiana", "la"),
+    ("oklahoma", "ok"),
+    ("arizona", "az"),
+    ("maine", "me"),
+    ("oregon", "or"),
+    ("arkansas", "ar"),
+    ("maryland", "md"),
+    ("pennsylvania", "pa"),
+    ("american samoa", "as"),
+    ("massachusetts", "ma"),
+    ("puerto rico", "pr"),
+    ("california", "ca"),
+    ("michigan", "mi"),
+    ("rhode island", "ri"),
+    ("colorado", "co"),
+    ("minnesota", "mn"),
+    ("south carolina", "sc"),
+    ("connecticut", "ct"),
+    ("mississippi", "ms"),
+    ("south dakota", "sd"),
+    ("delaware", "de"),
+    ("missouri", "mo"),
+    ("tennessee", "tn"),
+    ("district of columbia", "dc"),
+    ("montana", "mt"),
+    ("texas", "tx"),
+    ("florida", "fl"),
+    ("nebraska", "ne"),
+    ("trust territories", "tt"),
+    ("georgia", "ga"),
+    ("nevada", "nv"),
+    ("utah", "ut"),
+    ("guam", "gu"),
+    ("new hampshire", "nh"),
+    ("vermont", "vt"),
+    ("hawaii", "hi"),
+    ("new jersey", "nj"),
+    ("virginia", "va"),
+    ("idaho", "id"),
+    ("new mexico", "nm"),
+    ("virgin islands", "vi"),
+    ("illinois", "il"),
+    ("new york", "ny"),
+    ("washington", "wa"),
+    ("indiana", "in"),
+    ("north carolina", "nc"),
+    ("west virginia", "wv"),
+    ("iowa", "ia"),
+    ("north dakota", "nd"),
+    ("wisconsin", "wi"),
+    ("kansas", "ks"),
+    ("northern mariana islands", "mp"),
+    ("wyoming", "wy"),
+]
+
+_FULL2ABBR_MAP = {full: abbr for full, abbr in _US_STATE_TUPLES}
+_ABBR2FULL_MAP = {abbr: full for full, abbr in _US_STATE_TUPLES}
+
+
+def full2abbr(full): return _FULL2ABBR_MAP.get(full)
+def abbr2full(abbr): return _ABBR2FULL_MAP.get(abbr)
 
 
 # this is for osm geocoder api
@@ -18,11 +97,6 @@ class ParsedAddress:
         country: Optional[str] = None,
         postalcode: Optional[str] = None,
     ):
-        # _args = [name, street, city, state, country, postalcode]
-        # if not any(_args):
-        #     raise Exception("bad address")
-
-        self.input = input
         self.name = name
         self.street = street
         self.city = city
@@ -34,72 +108,134 @@ class ParsedAddress:
         return json.dumps(self.__dict__, indent=4)
 
 
-_name_regex = re.compile(r'^[^\d]+')
-_po_box_regex_no_num = re.compile(r'p\.?o\.?\s+box', re.IGNORECASE)
-_po_box_regex = re.compile(r'p\.?o\.?\s+box\s+[a-z\-]*\d+', re.IGNORECASE)
-_street_term_regex = re.compile(
-    r'.*(street|avenue|boulevard|highway|st|ave|av|blvd)[\.,]?',
-    re.IGNORECASE
-)
+class AddressParser:
+    """
+    Parse addresses into component parts
+    """
+    _fp = 'lib/data/country_subcountry_city.csv'
+    _city_col = 'name'
+    _country_col = 'country'
+    _country_code_col = 'country_code'
+    _state_col = 'subcountry'
+    _geonameid = '_geonameid'
+    _df = pd.read_csv(_fp)
+    _df.fillna('', inplace=True)
 
+    countries = _df[_country_col].unique()
 
-def _parse_name(address: str) -> Optional[str]:
-    global _name_regex, _po_box_regex
-    mat = _name_regex.match(address)
-    if mat:
-        out = mat.group()
-        out = _po_box_regex_no_num.sub('', out).strip()
-        return out
-    return None
+    all_cities = _df[_city_col].unique()
+    us_cities = _df.loc[_df[_country_code_col] == 'USA'][_city_col]
+    non_us_cities = _df.loc[_df[_country_code_col] != 'USA'][_city_col]
 
+    states_us_full = [full for full, abbr in _US_STATE_TUPLES]
+    states_us_abbr = [abbr for full, abbr in _US_STATE_TUPLES]
+    states_non_us = _df.loc[_df[_country_code_col] != 'USA'].as_list()
 
-def _parse_street(address: str) -> Optional[str]:
-    global _name_regex, _po_box_regex
-    pob_mat = _po_box_regex.search(address)
-    if pob_mat:
-        return pob_mat.group().strip()
-    # get rid of name
-    stripped = _name_regex.sub('', address).strip()
-    mat = _street_term_regex.match(stripped)
-    if mat:
-        return mat.group()
-    return None
+    # def _make_regex(list_of_strings) -> re.Pattern:
+    #     return re.compile(
+    #         r'\b(' + r'|'.join(list_of_strings) + r')\b',
+    #         re.IGNORECASE
+    #     )
+    # US_CITY_REGEX = _make_regex(us_cities)
 
+    US_CITY_REGEX = re.compile(
+        r'\b(' + r'|'.join(us_cities) + r')\b',
+        re.IGNORECASE
+    )
 
-def parse_non_us_address(_input) -> ParsedAddress:
-    res = ParsedAddress(safe_clean_string(_input))
-    res.country = JParse.parse_country(res.input)
-    res.city = JParse.parse_city(res.input)
-    res.state = JParse.parse_state(res.input)
-    res.postalcode = reverse_zip_parse(res.input)
-    res.name = _parse_name(res.input)
-    res.street = _parse_street(res.input)
-    return res
+    # TODO make per country/state
+    NON_US_CITY_REGEX = re.compile(
+        r'\b(' + r'|'.join(non_us_cities) + r')\b',
+        re.IGNORECASE
+    )
 
+    FULL_US_STATE_REGEX = re.compile(
+        r'\b(' + r'|'.join(states_us_full) + r')\b',
+        re.IGNORECASE
+    )
 
-def parse_us_address(_input: Optional[str]) -> ParsedAddress:
-    res = ParsedAddress(safe_clean_string(_input))
-    res.country = 'usa'
-    res.postalcode = reverse_zip_parse(res.input)
-    res.state = UsStateTools.parse_state(res.input)
-    res.name = _parse_name(res.input)
-    res.street = _parse_street(res.input)
-    res.city = JParse.parse_city(res.input)
-    return res
+    ABBR_US_STATE_REGEX = re.compile(
+        r'\s+(' + '|'.join(states_us_abbr) + r')\s+',
+        re.IGNORECASE
+    )
 
+    NON_US_STATE_REGEX = re.compile(
+        r'\s+(' + '|'.join(states_non_us) + r')\s+',
+        re.IGNORECASE
+    )
 
-def _by_whitespace(_input: str) -> list[str]:
-    return re.split(r'\s+', _input)
+    @classmethod
+    def parse_name(cls, address) -> Optional[str]:
+        mat = _NAME_REGEX.match(address)
+        if mat:
+            out = mat.group()
+            out = _PO_BOX_REGEX_NO_NUM.sub('', out).strip()
+            if out:
+                return out
+        return None
+
+    @classmethod
+    def parse_street(cls, address: str) -> Optional[str]:
+        pob_mat = _PO_BOX_REGEX.search(address)
+        if pob_mat:
+            return pob_mat.group().strip()
+        # get rid of name
+        stripped = _NAME_REGEX.sub('', address).strip()
+        mat = _STREET_TERM_REGEX.match(stripped)
+        if mat:
+            return mat.group()
+        return None
+
+    @classmethod
+    def parse_city(cls, address) -> Optional[str]:
+        raise NotImplementedError()
+
+    @classmethod
+    def parse_country(cls, address) -> Optional[str]:
+        raise NotImplementedError()
+
+    @classmethod
+    def parse_state(cls, address) -> Optional[str]:
+        """tries full, then abbrev, always returns abbreviation"""
+        full_res = cls._parse_state_full(address)
+        if full_res is not None:
+            return cls.full2abbr(full_res)
+        abb_res = cls._parse_state_abbr(address)
+        if abb_res is None:
+            print('couldnt find state in address {}'.format(address))
+        return abb_res
+
+    @classmethod
+    def parse_zip(cls, address) -> Optional[str]:
+        return cls._reverse_zip_parse(address)
+
+    @classmethod
+    def _reverse_zip_parse(addr) -> Optional[str]:
+        """
+        this parses an address for a zip code from the right
+        """
+        rev = addr[::-1]
+        mat = re.search(_REVERSE_ZIP_REGEX, rev)
+        if mat:
+            return mat[0][::-1]
+        return None
+
+    @classmethod
+    def _parse_state_full(cls, addr) -> Optional[str]:
+        mat = re.search(cls._FULL_STATE_REGEX, addr)
+        if mat:
+            return mat[0].strip()
+        return None
+
+    @classmethod
+    def _parse_state_abbr(cls, addr) -> Optional[str]:
+        mat = re.search(cls._ABBR_STATE_REGEX, addr)
+        if mat:
+            return mat[0].strip()
+        return None
 
 
 if __name__ == '__main__':
-    # import pandas as pd
-    # fp = 'world-cities.csv'
-    # df = pd.read_csv(fp)
-    # import re
-    # df.columns
-    # df = df.fillna('')
-
     import pandas as pd
     fp = '~/Public/Datasets/offshore-leaks-db/nodes-addresses.csv'
     df = pd.read_csv(fp)
--- jtool/utils.py
+++ jtool/utils.py
@@ -1,186 +1,11 @@
-from typing import List, Optional
-
 import re
 
 import pandas as pd
 
 
-class UsStateTools:
-    tups = [
-        ("alabama", "al"),
-        ("kentucky", "ky"),
-        ("ohio", "oh"),
-        ("alaska", "ak"),
-        ("louisiana", "la"),
-        ("oklahoma", "ok"),
-        ("arizona", "az"),
-        ("maine", "me"),
-        ("oregon", "or"),
-        ("arkansas", "ar"),
-        ("maryland", "md"),
-        ("pennsylvania", "pa"),
-        ("american samoa", "as"),
-        ("massachusetts", "ma"),
-        ("puerto rico", "pr"),
-        ("california", "ca"),
-        ("michigan", "mi"),
-        ("rhode island", "ri"),
-        ("colorado", "co"),
-        ("minnesota", "mn"),
-        ("south carolina", "sc"),
-        ("connecticut", "ct"),
-        ("mississippi", "ms"),
-        ("south dakota", "sd"),
-        ("delaware", "de"),
-        ("missouri", "mo"),
-        ("tennessee", "tn"),
-        ("district of columbia", "dc"),
-        ("montana", "mt"),
-        ("texas", "tx"),
-        ("florida", "fl"),
-        ("nebraska", "ne"),
-        ("trust territories", "tt"),
-        ("georgia", "ga"),
-        ("nevada", "nv"),
-        ("utah", "ut"),
-        ("guam", "gu"),
-        ("new hampshire", "nh"),
-        ("vermont", "vt"),
-        ("hawaii", "hi"),
-        ("new jersey", "nj"),
-        ("virginia", "va"),
-        ("idaho", "id"),
-        ("new mexico", "nm"),
-        ("virgin islands", "vi"),
-        ("illinois", "il"),
-        ("new york", "ny"),
-        ("washington", "wa"),
-        ("indiana", "in"),
-        ("north carolina", "nc"),
-        ("west virginia", "wv"),
-        ("iowa", "ia"),
-        ("north dakota", "nd"),
-        ("wisconsin", "wi"),
-        ("kansas", "ks"),
-        ("northern mariana islands", "mp"),
-        ("wyoming", "wy"),
-    ]
-
-    _FULL2ABBR_MAP = {full: abbr for full, abbr in tups}
-    _ABBR2FULL_MAP = {abbr: full for full, abbr in tups}
-
-    _FULL_STATE_REGEX = re.compile(
-        r'\s+(' + '|'.join(full for full, abb in tups) + r')\s+',
-        re.IGNORECASE
-    )
-
-    _ABBR_STATE_REGEX = re.compile(
-        r'\s+(' + '|'.join(abb for full, abb in tups) + r')\s+',
-        re.IGNORECASE
-    )
-
-    @classmethod
-    def abbrs(cls) -> List[str]:
-        return [abb for full, abb in cls.tups]
-
-    @classmethod
-    def fulls(cls) -> List[str]:
-        return [full for full, abb in cls.tups]
-
-    @classmethod
-    def full2abbr(cls, full): return cls._FULL2ABBR_MAP.get(full)
-
-    @classmethod
-    def abbr2full(cls, abbr): return cls._ABBR2FULL_MAP.get(abbr)
-
-    @classmethod
-    def parse_state(cls, addr) -> Optional[str]:
-        """always returns abbreviation"""
-        full_res = cls._parse_full(addr)
-        if full_res is not None:
-            return cls.full2abbr(full_res)
-        abb_res = cls._parse_abbr(addr)
-        if abb_res is None:
-            print('couldnt find state in address {}'.format(addr))
-        return abb_res
-
-    @classmethod
-    def _parse_full(cls, addr) -> Optional[str]:
-        mat = re.search(cls._FULL_STATE_REGEX, addr)
-        if mat:
-            return mat[0].strip()
-        return None
-
-    @classmethod
-    def _parse_abbr(cls, addr) -> Optional[str]:
-        mat = re.search(cls._ABBR_STATE_REGEX, addr)
-        if mat:
-            return mat[0].strip()
-        return None
-
-
-class AddressParser:
-    """
-    Parse addresses into component parts
-    """
-    _fp = 'lib/data/country_subcountry_city.csv'
-    _city_col = 'name'
-    _country_col = 'country'
-    _country_code_col = 'country_code'
-    _state_col = 'subcountry'
-    _geonameid = '_geonameid'
-    _df = pd.read_csv(_fp)
-    _df.fillna('', inplace=True)
-
-    countries = [v.lower() for v in _df[_country_col].unique()]
-    cities = [v.lower() for v in _df[_city_col].unique()]
-    states_us_full = [full for full, abbr in UsStateTools.tups]
-    states_us_abbr = [abbr for full, abbr in UsStateTools.tups]
-    states_non_us = _df.loc[_df[_country_code_col] != 'USA']
-
-    COUNTRY_REGEX = re.compile(
-        r'\b(' + r'|'.join(countries) + r')\b',
-        re.IGNORECASE
-    )
-
-    # TODO make per country/state
-    CITY_REGEX = re.compile(
-        r'\b(' + r'|'.join(cities) + r')\b',
-        re.IGNORECASE
-    )
-
-    FULL_US_STATE_REGEX = re.compile(
-        r'\b(' + r'|'.join(states_us_full) + r')\b',
-        re.IGNORECASE
-    )
-
-    ABBR_US_STATE_REGEX = re.compile(
-        r'\s+(' + '|'.join(states_us_abbr) + r')\s+',
-        re.IGNORECASE
-    )
-
-    NON_US_STATE_REGEX = re.compile(
-        r'\s+(' + '|'.join(states_non_us) + r')\s+',
-        re.IGNORECASE
-    )
-
-    @classmethod
-    def parse_city(cls, address) -> Optional[str]:
-        raise NotImplementedError()
-
-    @classmethod
-    def parse_country(cls, address) -> Optional[str]:
-        raise NotImplementedError()
-
-    @classmethod
-    def parse_full_state(cls, address) -> Optional[str]:
-        raise NotImplementedError()
-
-
 _SPACE_REGEX = re.compile(r'\s\s+')
-_USA_REGEX = re.compile(r'\s*(usa|u s a|united states of america)\s+(si)?')
+
 _REMOVE_TO_CLEAN_REGEX = re.compile(r'[^a-z0-9 ]')
-_REVERSE_ZIP_REGEX = re.compile(r'(\d\d\d\d\-?)? ?(\d\d\d\d\d)')
 
 
 def safe_clean_string(s):
@@ -206,17 +31,6 @@ def clean_dataframe_strings(df, subset=None):
     return df
 
 
-def reverse_zip_parse(addr):
-    """
-    this parses an address for a zip code from the right
-    """
-    global _REVERSE_ZIP_REGEX
-    rev = addr[::-1]
-    mat = re.search(_REVERSE_ZIP_REGEX, rev)
-    if mat:
-        return mat[0][::-1]
-
-
 def nan2none(d):
     """
     recurses through dict changing NaN to None
@@ -229,33 +43,36 @@ def nan2none(d):
     return d
 
 
-class DataTools:
-    OFFSHORE_LEAKS_COLUMNS = [
-        "name",
-        "lat",
-        "long",
-        "node_id",
-        "address",
-        "_id",
-        "original_name",
-        "former_name",
-        "jurisdiction",
-        "jurisdiction_description",
-        "company_type",
-        "internal_id",
-        "incorporation_date",
-        "inactivation_date",
-        "struck_off_date",
-        "dorm_date",
-        "status",
-        "service_provider",
-        "ibcRUC",
-        "country_codes",
-        "countries",
-        "sourceID",
-        "valid_until",
-        "note",
-        "zip",
-        "zip_five",
-        "state"
-    ]
+def split_by_whitespace(_input: str) -> list[str]:
+    return re.split(r'\s+', _input)
+
+
+OFFSHORE_LEAKS_COLUMNS = [
+    "name",
+    "lat",
+    "long",
+    "node_id",
+    "address",
+    "_id",
+    "original_name",
+    "former_name",
+    "jurisdiction",
+    "jurisdiction_description",
+    "company_type",
+    "internal_id",
+    "incorporation_date",
+    "inactivation_date",
+    "struck_off_date",
+    "dorm_date",
+    "status",
+    "service_provider",
+    "ibcRUC",
+    "country_codes",
+    "countries",
+    "sourceID",
+    "valid_until",
+    "note",
+    "zip",
+    "zip_five",
+    "state"
+]
