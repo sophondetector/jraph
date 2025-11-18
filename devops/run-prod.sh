8d6eb1aee65206d1a1813fcb8095b897fdf96d3e
sophondetector <nateisgood@gmail.com>
2025-11-18 14:10:40 -0500

    add TODO

--- devops/run-prod.sh
+++ devops/run-prod.sh
@@ -5,14 +5,18 @@ EXTRA_FILES_ARR=(
 	static/style.css
 	static/leaflet.filelayer.js
 	static/togeojson.js
-) 
-EXTRA_FILES=$(IFS=: ; echo "${EXTRA_FILES_ARR[*]}")
+)
+EXTRA_FILES=$(
+	IFS=:
+	echo "${EXTRA_FILES_ARR[*]}"
+)
 HOST="0.0.0.0"
 PORT=80
 RUN_DIR=/root/jraph
 
 cd $RUN_DIR
 
+#TODO change this to run with gunicord in prod
 flask run \
 	--extra-files "$EXTRA_FILES" \
 	--port $PORT \
