5c769babcfaa242bbf65ab99fba5e9a3d04dd071
sophondetector <nateisgood@gmail.com>
2025-11-18 21:05:11 -0500

    get rid of unneeded lines in run-dev.sh

--- devops/run-dev.sh
+++ devops/run-dev.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-EXTRA_FILES_ARR=(
-	templates/index.html
-	static/style.css
-	static/leaflet.filelayer.js
-	static/togeojson.js
-)
-EXTRA_FILES=$(
-	IFS=:
-	echo "${EXTRA_FILES_ARR[*]}"
-)
 HOST="localhost"
 PORT=4000
 
 flask run \
 	--debug \
-	--extra-files $EXTRA_FILES \
 	--port $PORT \
 	--host $HOST
